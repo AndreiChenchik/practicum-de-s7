909fb3d7dd39ecee4c188320e906c41602c344b2
Andrei Chenchik <andrei@chenchik.me>
2022-08-05 18:39:47 +0200

    3.6.2

--- Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 2/realization.py
+++ Тема 3. Знакомство со Spark/6. Подключение к Spark/Задание 2/realization.py
@@ -1,2 +1,11 @@
 from pyspark.sql import SparkSession
-spark = # дополните код
+
+spark = (
+    SparkSession.builder.master(  # создаём объект Spark-сессии, обращаясь к объекту builder, который создаёт сессию, учитывая параметры конфигурации
+        "yarn"  # явно указываем, что хотим запустить Spark в локальном режиме
+    )
+    .appName("My second session")  # задаём название нашего Spark-приложения
+    .getOrCreate()
+)
+
+# Двигайтесь дальше! Ваш код: kMCwkWEe1W
