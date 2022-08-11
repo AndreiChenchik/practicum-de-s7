ddd915d8d1d6101aba69b66f09a6e1be3645f3e9
Andrei Chenchik <andrei@chenchik.me>
2022-08-11 09:38:49 +0200

    4.7

---
+++ Тема 4. PySpark для инженера данных/7. Операции с DataFrame кэширование и контрольные точки/Задание 2/realization.py
@@ -0,0 +1,52 @@
+import findspark
+
+findspark.init()
+findspark.find()
+
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder.master("local")
+    .appName("Learning DataFrames")
+    .getOrCreate()
+)
+# данные первого датафрейма
+book = [
+    ("Harry Potter and the Goblet of Fire", "J. K. Rowling", 322),
+    ("Nineteen Eighty-Four", "George Orwell", 382),
+    ("Jane Eyre", "Charlotte Brontë", 159),
+    ("Catch-22", "Joseph Heller", 174),
+    ("The Catcher in the Rye", "J. D. Salinger", 168),
+    ("The Wind in the Willows", "Kenneth Grahame", 259),
+    ("The Mayor of Casterbridge", "Thomas Hardy", 300),
+    ("Bad Girls", "Jacqueline Wilson", 299),
+]
+# данные второго датафрейма
+library = [
+    (322, "1"),
+    (250, "2"),
+    (400, "2"),
+    (159, "1"),
+    (382, "2"),
+    (322, "1"),
+]
+# названия атрибутов
+columns = ["title", "author", "book_id"]
+columns_library = ["book_id", "Library_id"]
+# создаём датафреймы
+df = spark.createDataFrame(data=book, schema=columns)
+df_library = spark.createDataFrame(data=library, schema=columns_library)
+# напишите ваш код ниже
+
+df_joined = df.join(
+    df_library, df.book_id == df_library.book_id, how="left_anti"
+)
+
+print(df_joined.select("title").explain())
+print(df_joined.cache().explain())
+
+spark.sparkContext.setCheckpointDir("/user/andrei/analytics/test_check")
+print(df_joined.checkpoint().explain())
+
+# Двигайтесь дальше! Ваш код: einWLzgTYR
